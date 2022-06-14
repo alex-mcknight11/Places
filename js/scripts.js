c9ba1b2ce44a0eb067223fec4b1b20ed74845a7b
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-06-14 15:05:52 -0700

    initialize repository for Places

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Business Logic for Place Tracker
+function PlaceTracker(){
+  this.Places = {};
+  this.currentID = 0;
+}
+
+PlaceTracker.prototype.addPlace = function(place) {
+place.id = this.assignID();
+this.Places[place.id] = place;
+};
+
+PlaceTracker.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+// Business Logic for Places
+function Places(location, landmarks, time) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.time = time;
+};
+
+Places.prototype.PlaceEntry = function() {
+  return "I went to" + this.location + "and saw" + this.landmarks + "during" + this.time + ".";
+};
+
+//let placetracker = new PlaceTracker();
+let destination = new Places("brazil", "bigjesus", "september");
+PlaceTracker.addplace(destination);
+PlaceTracker.Places;
